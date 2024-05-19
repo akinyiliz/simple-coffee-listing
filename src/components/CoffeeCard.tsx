3aa55e4cf924a70b7bca57e29774730f780aa95e
akinyiliz <lizzieke84@gmail.com>
2024-05-19 17:33:09 +0300

    feat: Add CoffeeCard component

---
+++ src/components/CoffeeCard.tsx
@@ -0,0 +1,64 @@
+import { FC } from "react";
+
+import Star from "../assets/Star.svg";
+import { Coffee } from "../types/Coffee";
+import RatingStar from "../assets/Star_fill.svg";
+
+const CoffeeCard: FC<Coffee> = ({
+  id,
+  name,
+  image,
+  available,
+  price,
+  popular,
+  rating,
+  votes,
+}) => {
+  return (
+    <div key={id} className="flex flex-col gap-3">
+      <div className="relative">
+        <img src={image} alt={name} className="rounded-2xl" />
+        {popular && (
+          <small className="absolute top-2 left-2 bg-[#F6C768] py-1 px-3 text-[10px] text-black font-bold rounded-xl">
+            Popular
+          </small>
+        )}
+      </div>
+
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">{name}</h2>
+
+          <p className="bg-[#BEE3CC] text-black text-[12px] text-center font-semibold p-1 rounded-md">
+            {price}
+          </p>
+        </div>
+
+        <div className="flex flex-row items-center justify-between">
+          {rating ? (
+            <div className="flex flex-row items-center">
+              <img src={RatingStar} alt="Yellow Star Rating" />
+              <p className="text-[14px] font-semibold">
+                {rating}{" "}
+                <span className="text-[#6F757C] text-[14px]">
+                  ({votes} votes)
+                </span>
+              </p>
+            </div>
+          ) : (
+            <div className="flex flex-row items-center">
+              <img src={Star} alt="Star Rating" />
+              <p className="text-[#6F757C] text-[14px] font-semibold">
+                No ratings
+              </p>
+            </div>
+          )}
+
+          {!available && <span className="text-red-500">Sold Out</span>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
