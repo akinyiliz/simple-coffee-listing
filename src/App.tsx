cd5005912bc458d3d0cc121dad208286e7a2fe1c
akinyiliz <lizzieke84@gmail.com>
2024-05-19 17:26:03 +0300

    feat: Implement responsive background section in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Main from "./components/Main";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-500 text-3xl">Simple Coffee Listing</h1>
-    </>
+    <div className="relative w-full h-[200px] lg:h-[300px] bg-cafe-image bg-cover bg-no-repeat bg-center">
+      <div className="absolute top-32 lg:top-40 inset-x-0 w-[90%] h-full mx-auto lg:max-w-6xl">
+        <Main />
+      </div>
+    </div>
   );
 }
 
